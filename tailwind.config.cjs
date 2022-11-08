8faecdf320eba899b4fc697dd06472e52dbf0889
Camilo Suarez <camiloandressuarez1@gmail.com>
2022-11-08 12:48:38 -0500

    :truck: update bg image x2 / portfolio

--- tailwind.config.cjs
+++ tailwind.config.cjs
@@ -19,7 +19,7 @@ module.exports = {
         },
     },
       backgroundImage: {
-        "home-background": "url('./home.png')",
+        "home-background": "url('/home.png')",
       },
       colors: {
         'headerColor': "#1F2022",
